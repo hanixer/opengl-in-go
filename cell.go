e1c2b5cf65fd47ffa657ec3937ff0e9bb7b6cddb
My Name Is My Name <jijer@jiji.com>
2019-05-20 23:39:37 +0300

    basic drawing of a picture

---
+++ cell.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+	rows    = 10
+	columns = 10
+)
+
+var (
+	triangle = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+	}
+	square = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+
+		0.5, -0.5, 0,
+		0.5, 0.5, 0,
+		-0.5, 0.5, 0,
+	}
+)
+
+type cell struct {
+	drawable  uint32
+	alive     bool
+	aliveNext bool
+	x         int
+	y         int
+}
+
+type cellsSet [][]*cell
+
+func (c *cell) draw() {
+	fmt.Println("IJIJIJ")
+	gl.BindVertexArray(c.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
+}
+
+func (c *cell) updateState(cells cellsSet) {
+
+}
+
+func drawAllsells(cells cellsSet, window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if (i%2 != 0) == (j%2 == 0) {
+				cells[i][j].draw()
+			}
+		}
+	}
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func makeCells() cellsSet {
+	cells := make(cellsSet, rows)
+	for r := 0; r < rows; r++ {
+		cells[r] = make([]*cell, columns)
+		for c := 0; c < columns; c++ {
+			cells[r][c] = newCell(c, r)
+		}
+	}
+
+	return cells
+}
+
+func newCell(x, y int) *cell {
+	points := make([]float32, len(square))
+	var xScale float32 = 1.0 / columns
+	var yScale float32 = 1.0 / rows
+	for i := 0; i < len(square); i += 3 {
+		points[i] = (float32(square[i])+0.5+float32(x))*xScale*2.0 - 1.0
+		points[i+1] = (float32(square[i+1])+0.5+float32(y))*yScale*2.0 - 1.0
+	}
+	return &cell{
+		drawable: makeVao(points),
+		x:        x,
+		y:        y,
+	}
+}
--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
 	"log"
+	"os"
 	"runtime"
 	"strings"
+	"unsafe"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -13,72 +19,68 @@ import (
 const (
 	width              = 500
 	height             = 500
-	rows               = 10
-	columns            = 10
 	vertexShaderSource = `
-		#version 410
-		in vec3 vp;
-		void main() {
-			gl_Position = vec4(vp, 1.0);
-		}
-	` + "\x00"
+#version 410
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+
+out vec2 texCoord;
+
+void main() {
+	gl_Position = vec4(aPos, 1.0);
+	texCoord = aTexCoord;
+}
+` + "\x00"
+
 	fragmentShaderSource = `
-		#version 410
-		out vec4 frag_colour;
-		void main() {
-			frag_colour = vec4(1, 1, 1, 1);
-		}
-	` + "\x00"
+#version 410
+in vec2 texCoord;
+
+out vec4 fragColor;
+
+uniform sampler2D ourTexture;
+void main() {
+	fragColor = texture(ourTexture, texCoord);
+	// fragColor = vec4(0.3, 0.3, 0.5, 1);
+}
+` + "\x00"
 )
 
 var (
-	triangle = []float32{
-		-0.5, 0.5, 0,
-		-0.5, -0.5, 0,
-		0.5, -0.5, 0,
+	vertexData = []float32{
+		1.0, 1.0, 0.0, 1.0, 1.0, // top right
+		1.0, -1.0, 0.0, 1.0, 0.0, // bottom right
+		-1.0, -1.0, 0.0, 0.0, 0.0, // bottom left
+		-1.0, 1.0, 0.0, 0.0, 1.0, // top left
 	}
-	square = []float32{
-		-0.5, 0.5, 0,
-		-0.5, -0.5, 0,
-		0.5, -0.5, 0,
-
-		0.5, -0.5, 0,
-		0.5, 0.5, 0,
-		-0.5, 0.5, 0,
+	indices = []uint32{
+		0, 1, 3,
+		1, 2, 3,
 	}
 )
 
-type cell struct {
-	drawable  uint32
-	alive     bool
-	aliveNext bool
-	x         int
-	y         int
-}
-
-type cellsSet [][]*cell
-
-func (c *cell) draw() {
-	gl.BindVertexArray(c.drawable)
-	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
-}
-
-func (c *cell) updateState(cells cellsSet) {
-
-}
-
 func main() {
 	runtime.LockOSThread()
-
 	window := initGlfw()
 	defer glfw.Terminate()
+	program := initOpenGL()
+	vao := makeVao(vertexData)
+	texture := makeTexture()
 
-	prog := initOpenGL()
-
-	cells := makeCells()
+	gl.UseProgram(program)
 
 	for !window.ShouldClose() {
-		draw(cells, window, prog)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.UseProgram(program)
+
+		// draw(window, prog)
+		gl.ActiveTexture(gl.TEXTURE0)
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+		gl.BindVertexArray(vao)
+		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+		glfw.PollEvents()
+		window.SwapBuffers()
 	}
 }
 
@@ -93,7 +95,7 @@ func initGlfw() *glfw.Window {
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
+	window, err := glfw.CreateWindow(width, height, "Opengl thing", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -110,7 +112,7 @@ func initOpenGL() uint32 {
 	log.Println("Opengl version", version)
 	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
 	if err != nil {
@@ -123,20 +125,53 @@ func initOpenGL() uint32 {
 	return prog
 }
 
-func draw(cells cellsSet, window *glfw.Window, program uint32) {
+func draw123(window *glfw.Window, program uint32, vao uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
 
-	for i := 0; i < rows; i++ {
-		for j := 0; j < columns; j++ {
-			if (i%2 != 0) == (j%2 == 0) {
-				cells[i][j].draw()
-			}
-		}
-	}
+	gl.BindVertexArray(vao)
+	gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.Ptr(0))
 
 	glfw.PollEvents()
 	window.SwapBuffers()
+}
+
+func makeTexture() uint32 {
+	f, _ := os.Open("1.png")
+	img2, _ := png.Decode(f)
+
+	img := image.NewRGBA(img2.Bounds())
+	draw.Draw(img, img.Bounds(), img2, image.ZP, draw.Src)
+
+	height := img.Bounds().Dy()
+	stride := make([]uint8, img.Stride)
+	for row := 0; row < height/2; row++ {
+		mirrorRow := height - row - 1
+		strideCurrent := img.Pix[row*img.Stride : row*img.Stride+img.Stride]
+		strideMirror := img.Pix[mirrorRow*img.Stride : mirrorRow*img.Stride+img.Stride]
+		copy(stride, strideCurrent)
+		copy(strideCurrent, strideMirror)
+		copy(strideMirror, stride)
+	}
+
+	c := image.NewUniform(color.RGBA{30, 100, 40, 255})
+	for i := 0; i < 20; i += 2 {
+		r := image.Rect(30, i+10, 60, i+20)
+		draw.Draw(img, r, c, image.ZP, draw.Src)
+	}
+
+	ptr := &img.Pix[0]
+	var texture uint32
+	gl.GenTextures(1, &texture)
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+		int32(img.Bounds().Dx()), int32(img.Bounds().Dy()), 0,
+		gl.RGBA, gl.UNSIGNED_BYTE, unsafe.Pointer(ptr))
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+	return texture
 }
 
 func makeVao(points []float32) uint32 {
@@ -145,12 +180,20 @@ func makeVao(points []float32) uint32 {
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
 
+	var ebo uint32
+	gl.CreateBuffers(1, &ebo)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(indices), gl.Ptr(indices), gl.STATIC_DRAW)
+
 	var vao uint32
 	gl.GenVertexArrays(1, &vao)
 	gl.BindVertexArray(vao)
-	gl.EnableVertexAttribArray(0)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
 
 	return vao
 }
@@ -175,31 +218,4 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 	}
 
 	return shader, nil
-}
-
-func makeCells() cellsSet {
-	cells := make(cellsSet, rows)
-	for r := 0; r < rows; r++ {
-		cells[r] = make([]*cell, columns)
-		for c := 0; c < columns; c++ {
-			cells[r][c] = newCell(c, r)
-		}
-	}
-
-	return cells
-}
-
-func newCell(x, y int) *cell {
-	points := make([]float32, len(square))
-	var xScale float32 = 1.0 / columns
-	var yScale float32 = 1.0 / rows
-	for i := 0; i < len(square); i += 3 {
-		points[i] = (float32(square[i])+0.5+float32(x))*xScale*2.0 - 1.0
-		points[i+1] = (float32(square[i+1])+0.5+float32(y))*yScale*2.0 - 1.0
-	}
-	return &cell{
-		drawable: makeVao(points),
-		x:        x,
-		y:        y,
-	}
 }
